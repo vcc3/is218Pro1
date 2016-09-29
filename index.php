245c7e5f8af79f89e70c7e84c095502a8b10a709
vcc3 <vcc3@njit.edu>
2016-09-29 13:35:08 -0400

    added code for intro

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 
-
-echo "Hello world";
-
+// this prints text in php
+echo "Hello World";
+echo '<br>';
+// this is how to store value in a variable
+echo " Php intro program" ;
+echo '<br>';
+$myvar = "Some Text i want to store in a variable";
+// this shows the difference between single and double quotes.
+echo'$myvar';
+echo ' <br> ';
+echo "$myvar";
 
 
 
