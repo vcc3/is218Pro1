1a9db334d7d31ed51ba83d3caeb5752fa265ae7e
vcc3 <vcc3@njit.edu>
2016-09-29 13:50:57 -0400

    added arrays

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
 
-// this prints text in php
+// this prints text in php can also use print as well.
 echo "Hello World";
 echo '<br>';
 // this is how to store value in a variable
 echo " Php intro program" ;
 echo '<br>';
-$myvar = "Some Text i want to store in a variable";
+ $myvar = "Some Text i want to store in a variable";
 // this shows the difference between single and double quotes.
 echo'$myvar';
 echo ' <br> ';
 echo "$myvar";
-
-
-
-
-
-
+// two types of array. intergers for key, and dictionary for named keys
+// this is the example for php arrays
+ $myarray = array();
+ //$myarray[] is for pushing value into array.
+ $myarray[]= 'some value 1';
+ $myarray[]= 'some value 2';
+ $myarray[]= 'some value 3';
+ //print_r is used to view instead objects or arrays only!
+ print_r($myarray);
+// associative array  with named key
+ $myAssoc=  array('value 1' => $myarray, 'value 2' => $myarray);
+//example of nested array and Associative array with named keys
+print_r($myAssoc);
 
 
 ?>
